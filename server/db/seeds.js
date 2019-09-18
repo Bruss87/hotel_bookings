0b78dc3029b6d604666ba9da9dac9f5ea92c5e93
Karolina Kaczmarska <kaczmarska.kar@gmail.com>
2019-09-18 10:46:15 +0100

    basic structure setup

---
+++ server/db/seeds.js
@@ -0,0 +1,20 @@
+use hotel;
+db.dropDatabase();
+
+db.bookings.insertMany([
+    {
+        name: "Karolina Kaczmarska",
+        email: "karolina@email.com",
+        checkInStatus: "true" 
+    },
+    {
+        name: "Liliana Everett",
+        email: "liliana@email.com",
+        checkInStatus: "false" 
+    },
+    {
+        name: "Tom Hardy",
+        email: "tom@email.com",
+        checkInStatus: "false" 
+    },
+])
